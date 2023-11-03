442aa08f71490fec7c81d155ae5c67c57625a2e8
KeJunMao <kejun1997@gmail.com>
2023-11-03 16:01:43 +0800

    chore: add build done hook

--- build.config.ts
+++ build.config.ts
@@ -1,4 +1,5 @@
 import { defineBuildConfig } from 'unbuild'
+import { readFileSync, writeFileSync } from 'node:fs'
 
 export default defineBuildConfig({
   entries: ['src/index'],
@@ -8,4 +9,20 @@ export default defineBuildConfig({
     emitCJS: true,
   },
   failOnWarn: false,
+  hooks: {
+    'build:done'(ctx) {
+      if (ctx.options.stub) return
+      let file = 'dist/index.cjs'
+
+      let code = readFileSync(file, 'utf8')
+      code = code.replace('exports.default =', 'module.exports =')
+      code += 'exports.default = module.exports;'
+      writeFileSync(file, code)
+      file = 'dist/index.d.ts'
+      code = readFileSync(file, 'utf8')
+      code += 'export = uniHelper'
+      writeFileSync(file, code)
+      writeFileSync('dist/index.d.cts', code)
+    },
+  }
 })
