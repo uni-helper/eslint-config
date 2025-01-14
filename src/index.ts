0ea19ca0ccaa4f93ead96aa7227087a993af1885
Panpan <wurui.dev@gmail.com>
2025-01-14 17:08:19 +0800

    feat: add globals

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,11 @@ import type { Linter } from 'eslint'
 import type { FlatConfigComposer } from 'eslint-flat-config-utils'
 import type { Awaitable, OptionsConfig, TypedFlatConfigItem } from './types'
 import { antfu } from '@antfu/eslint-config'
+import { FlatCompat } from '@eslint/eslintrc'
 import { isPackageExists } from 'local-pkg'
 import { sortManifestJson, sortPagesJson, sortThemeJson, uni } from './configs'
+
+const compat = new FlatCompat()
 
 export * from './types'
 
@@ -38,6 +41,24 @@ export function uniHelper(
     options.vue = options.vue || true
     userConfigs.unshift(uni())
   }
+
+  userConfigs.push(compat.config({
+    globals: {
+      dd: 'readonly', // https://open.dingtalk.com/
+      jd: 'readonly', // https://mp.jd.com/
+      ks: 'readonly', // https://mp.kuaishou.com/
+      my: 'readonly', // https://opendocs.alipay.com/mini
+      plus: 'readonly', // http://www.html5plus.org/doc/h5p.html
+      qh: 'readonly', // https://mp.360.cn/
+      qq: 'readonly', // https://q.qq.com/
+      swan: 'readonly', // https://smartprogram.baidu.com/docs
+      tt: 'readonly', // https://developer.open-douyin.com/ https://open.feishu.cn/
+      uni: 'readonly', // https://uniapp.dcloud.io/
+      uniCloud: 'readonly', // https://uniapp.dcloud.io
+      weex: 'readonly', // https://weex.apache.org/
+      wx: 'readonly', // https://developers.weixin.qq.com/miniprogram/dev/framework/
+    },
+  }))
 
   return antfu(options, ...userConfigs)
 }
